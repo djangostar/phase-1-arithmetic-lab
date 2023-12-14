a8582b7f9a6d52152d6bfeb9d411df1baa843538
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2023-12-13 16:56:22 -0800

    wipe

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 // Write your code here
+
 //let multiply;
-let num1 = 31;
-let num2 = 2;
-let multiply = num1 * num2
 
-let random;
-random = Math.floor(Math.random() * 10);
 
-//let mod;
-let num3 = 4;
-let num4 = 5;
-let mod = num3 % num4;
+//let random
 
-let max = Math.max(0, 20);
+
+
+//let mod
+
+
+
+//let max
+
+
